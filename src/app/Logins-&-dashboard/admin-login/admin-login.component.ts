ea0c7a81d4dbd4195bcce48b0b45fe229974400e
Shubhanshi Agarwal <shubhanshi.agarwal@coxautoinc.com>
2023-08-30 21:00:07 +0530

    admin login and user login fields updated

--- src/app/Logins-&-dashboard/admin-login/admin-login.component.ts
+++ src/app/Logins-&-dashboard/admin-login/admin-login.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.component.html',
   styleUrls: ['./admin-login.component.css']
 })
-export class AdminLoginComponent {
+export class AdminLoginComponent implements OnInit {
+  adminLoginForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder, private router: Router) {
+    this.adminLoginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  adminLogin() {
+    // Implement your admin login logic here
+    
+    // After successful login, navigate to the admin dashboard
+    this.router.navigate(['/adminDashboard']);
+  }
 }
--- src/app/Logins-&-dashboard/user-login/user-login.component.ts
+++ src/app/Logins-&-dashboard/user-login/user-login.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-user-login',
   templateUrl: './user-login.component.html',
   styleUrls: ['./user-login.component.css']
 })
-export class UserLoginComponent {
-  constructor(private router: Router) {}
-
- 
+export class UserLoginComponent implements OnInit{
+  
+    loginForm: FormGroup;
+  
+    constructor(private formBuilder: FormBuilder,private router:Router) {
+      this.loginForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', Validators.required]
+      });
+    }
+  
+    ngOnInit(): void {
+    }
+    login() {
+      // Implement your login logic here
+      // After successful login, navigate to the user dashboard
+    this.router.navigate(['/userDashboard']);
+    }
 }
